cb5b8b3193cd8dc88d9cec42a0d06e828bba9534
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-10-13 14:42:33 +0530

    recursion

---
+++ recursion/recursion_assignment/src/com/masai/Main.java
@@ -0,0 +1,17 @@
+package com.masai;
+
+public class Main{
+
+    public static int factorial(int n){
+        if(n == 0 || n ==  1){
+            return 1;
+        }
+
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(factorial(5));
+
+    }
+}
