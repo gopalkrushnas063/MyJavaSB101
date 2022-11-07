ac8c3c6fe816b8ac06ae3527c815f26053acf3e7
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-11-07 23:04:29 +0530

    DAO JDBC Application updated

--- Question_3/src/com/masai/usecases/GetAllProduct.java
+++ Question_3/src/com/masai/usecases/GetAllProduct.java
@@ -2,12 +2,19 @@ package com.masai.usecases;
 
 import com.masai.dao.ProductDao;
 import com.masai.dao.ProductDaoImpl;
+import com.masai.exception.ProductException;
+import com.masai.model.Product;
+
+import java.util.List;
 
 public class GetAllProduct {
     public static void main(String[] args) {
         ProductDao dao = new ProductDaoImpl();
         try{
-            Lis
+            List<Product> products = dao.getAllProducts();
+            products.forEach(System.out::println);
+        }catch (ProductException e){
+            System.out.println(e.getMessage());
         }
     }
 }
